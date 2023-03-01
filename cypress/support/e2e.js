82eb827f1dee767a642ed7c6fa07094c3c7e6454
marcospereira01 <marcospereira2023@hotmail.com>
2023-03-01 12:00:59 -0400

    Configure Cypress Grep

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -2,3 +2,8 @@ import './commands'
 import 'cypress-iframe'
 import 'cypress-mailosaur'
 import 'cypress-file-upload'
+
+const { cypressGrep } = require('cypress-grep')
+module.exports = (on, config) => {
+  on('file:preprocessor', cypressGrep(config))
+}
