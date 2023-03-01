0c47dbe85588ead9cd0b85037b9f216932f13a22
marcospereira01 <marcospereira2023@hotmail.com>
2023-03-01 10:08:24 -0400

    configuração do dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "x35f2v",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
