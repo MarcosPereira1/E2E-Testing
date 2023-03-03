1fcab87d83f158161fe95cb72e4f7afb7f722751
marcospereira01 <marcospereira2023@hotmail.com>
2023-03-02 21:23:41 -0400

    implement dark

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,7 +1,9 @@
 import './commands'
+import 'cypress-dark'
 import 'cypress-iframe'
 import 'cypress-mailosaur'
 import 'cypress-file-upload'
+
 
 const { cypressGrep } = require('cypress-grep')
 module.exports = (on, config) => {
